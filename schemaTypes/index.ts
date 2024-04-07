7e7026689152bf661f9159b8a50838dca0ec3819
James Camilleri <james@james.mt>
2024-04-07 21:23:09 +0100

    Recreate schema extraction error condition

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,11 +1,23 @@
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+// Default import syntax, without "type": "module" in package.json and NodeNext resolution, work fine.
+//
+// import blockContent from './blockContent'
+// import crewMember from './crewMember'
+// import castMember from './castMember'
+// import movie from './movie'
+// import person from './person'
+// import screening from './screening'
+// import plotSummary from './plotSummary'
+// import plotSummaries from './plotSummaries'
+
+// Correcting the imports as dictated by TypeScript cause failures on schema extraction.
+import blockContent from './blockContent.js'
+import crewMember from './crewMember.js'
+import castMember from './castMember.js'
+import movie from './movie.js'
+import person from './person.js'
+import screening from './screening.js'
+import plotSummary from './plotSummary.js'
+import plotSummaries from './plotSummaries.js'
 
 export const schemaTypes = [
   // Document types
